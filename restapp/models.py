328b71c25f58e05fd30bb164f8028cee69ece1cb
Jumana Shaji <jahanashaji2020@gmail.com>
2021-11-08 10:48:43 +0530

    Table created

--- restapp/models.py
+++ restapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=200)
+    task_desc = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
